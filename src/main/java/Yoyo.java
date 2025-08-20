6c0627c663ab01d3700be7bce241b4336c073f72
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-20 20:33:57 +0800

    Update test case files
    Test cases now updated to match chatbot's
    intended result. Test result passes runtest.sh.
    
    Let's edit EXPECTED.txt to correctly output
    chatbot's message.

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -2,7 +2,7 @@ public class Yoyo {
     public static void main(String[] args) {
         String separator = "____________________________" +
                 "________________________________";
-        
+
         System.out.println(separator);
         System.out.println("Yo! The name's Yoyo.\nWhat can " +
                 "I do for you?");
