70e36a4f24266d6a0efc53eaadb1a6020bd2e282
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-20 21:54:25 +0800

    Update chatbot to add to a list
    A chatbot that can add to a list is a useful
    function that would eventually allow the chatbot
    to track a list of text which becomes more useful for
    the user.
    
    Let's,
    * use the user's prompt as input to add to a list
    * create a 'list' command that makes Yoyo print out
    the list it has collated so far
    * update test cases to test new functionality

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 
 public class Yoyo {
+    private static String[] lst = new String[100];
+    private static int currIdx = 0;
+
+    private static void addToLst(String text) {
+        lst[currIdx] = text;
+        currIdx++;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String separator = "=========================" +
                 "===============================";
         String tab = "    ";
@@ -13,20 +20,33 @@ public class Yoyo {
                 + tab + "What can I do for you?");
         System.out.println(tab + separator);
 
+        // Detects the user's command and performs the
+        // appropriate action with formatting
         while (true) {
             String userPrompt = scanner.nextLine();
 
-            if (!userPrompt.equals("bye")) {
+            if (userPrompt.equals("bye")) {
+                // Yoyo says bye before quitting
                 System.out.println(tab + separator);
-                System.out.println(tab + userPrompt);
+                System.out.println(tab + "Aww, bye! See ya later.");
+                System.out.println(tab + separator);
+                break;
+            } else if (userPrompt.equals("list")) {
+                // Yoyo lists out what it wrote in its list
+                System.out.println(tab + separator);
+                for (int idx = 0; idx < Yoyo.lst.length; idx++) {
+                    if (idx < currIdx) {
+                        System.out.println(tab + (idx + 1) + ". " + Yoyo.lst[idx]);
+                    }
+                }
                 System.out.println(tab + separator);
             } else {
-                break;
+                // Yoyo adds the userPrompt to its list
+                System.out.println(tab + separator);
+                Yoyo.addToLst(userPrompt);
+                System.out.println(tab + "added: " + userPrompt);
+                System.out.println(tab + separator);
             }
         }
-
-        System.out.println(tab + separator);
-        System.out.println(tab + "Aww, bye! See ya later.");
-        System.out.println(tab + separator);
     }
 }
