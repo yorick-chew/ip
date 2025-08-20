0266184bca69fce74972e4c1559f17b42636be17
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-20 21:05:11 +0800

    Update chatbot responses to echo user
    A chatbot that responds to user's commands opens
    the door for more complex commands to be implemented.
    
    Let's,
    * make the chatbot echo the user's commands
    * make the chatbot exit when user inputs "bye"

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Yoyo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String separator = "=========================" +
                 "===============================";
         String tab = "    ";
@@ -7,6 +11,20 @@ public class Yoyo {
         System.out.println(tab + separator);
         System.out.println(tab + "Yo! The name's Yoyo.\n"
                 + tab + "What can I do for you?");
+        System.out.println(tab + separator);
+
+        while (true) {
+            String userPrompt = scanner.nextLine();
+
+            if (!userPrompt.equals("bye")) {
+                System.out.println(tab + separator);
+                System.out.println(tab + userPrompt);
+                System.out.println(tab + separator);
+            } else {
+                break;
+            }
+        }
+
         System.out.println(tab + separator);
         System.out.println(tab + "Aww, bye! See ya later.");
         System.out.println(tab + separator);
