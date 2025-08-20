99217e95a741f8c91ed07abbab7b86c6c6fbfa4f
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-20 19:59:21 +0800

    Rename chatbot and personalise greeting
    The bot needs a new name and personality to
    stand out from other chatbots.
    
    Let's,
    * change the name of the chatbot to Yoyo
    * add a custom chatbot greeting and
    farewell message

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Yoyo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Yoyo.java
@@ -0,0 +1,13 @@
+public class Yoyo {
+    public static void main(String[] args) {
+        String separator = "____________________________" +
+                "________________________________";
+        
+        System.out.println(separator);
+        System.out.println("Yo! The name's Yoyo.\nWhat can " +
+                "I do for you?");
+        System.out.println(separator);
+        System.out.println("Aww, bye! See ya later.");
+        System.out.println(separator);
+    }
+}
