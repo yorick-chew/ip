c94207dcb4516f3894f5a4e5a92905a9d6faee1b
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-20 23:01:24 +0800

    Add ability to mark tasks on list as done
    Marking and unmarking tasks allows to-do lists to
    be tracked by Yoyo, providing more use cases for
    Yoyo to the user.
    
    Let's add 'mark' and 'unmark' commands to Yoyo with '[X]'
    beside marked tasks and '[ ]' beside unmarked tasks.

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Yoyo {
-    private static String[] lst = new String[100];
+    private static Task[] taskLst = new Task[100];
     private static int currIdx = 0;
 
     private static void addToLst(String text) {
-        lst[currIdx] = text;
+        taskLst[currIdx] = new Task(text);
         currIdx++;
     }
 
@@ -23,7 +23,7 @@ public class Yoyo {
         // Detects the user's command and performs the
         // appropriate action with formatting
         while (true) {
-            String userPrompt = scanner.nextLine();
+            String userPrompt = scanner.next();
 
             if (userPrompt.equals("bye")) {
                 // Yoyo says bye before quitting
@@ -34,11 +34,27 @@ public class Yoyo {
             } else if (userPrompt.equals("list")) {
                 // Yoyo lists out what it wrote in its list
                 System.out.println(tab + separator);
-                for (int idx = 0; idx < Yoyo.lst.length; idx++) {
+                for (int idx = 0; idx < Yoyo.taskLst.length; idx++) {
                     if (idx < currIdx) {
-                        System.out.println(tab + (idx + 1) + ". " + Yoyo.lst[idx]);
+                        System.out.println(tab + (idx + 1) + ". " + Yoyo.taskLst[idx]);
                     }
                 }
+                System.out.println(tab + separator);
+            } else if (userPrompt.equals("mark")) {
+                int taskIdx = scanner.nextInt() - 1;
+                Yoyo.taskLst[taskIdx].markAsDone();
+                System.out.println(tab + separator);
+                System.out.println(tab + "Oh man, you're clearing them tasks" +
+                                " like a pro!\n" + tab + "Marked it for you:\n" +
+                                tab + "   " + Yoyo.taskLst[taskIdx]);
+                System.out.println(tab + separator);
+            } else if (userPrompt.equals("unmark")) {
+                int taskIdx = scanner.nextInt() - 1;
+                Yoyo.taskLst[taskIdx].unmarkAsDone();
+                System.out.println(tab + separator);
+                System.out.println(tab + "Bruh... Alright fine, I won't judge!\n" +
+                                tab + "Unmarked it for you:\n" + tab + "   " +
+                                Yoyo.taskLst[taskIdx]);
                 System.out.println(tab + separator);
             } else {
                 // Yoyo adds the userPrompt to its list
