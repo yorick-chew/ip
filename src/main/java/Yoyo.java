569990328968b9d40986221cc53e4b3ae080160a
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-20 20:43:02 +0800

    Update appearance of user interface
    Clearer appearance of chatbot's messages
    using '=' as separator and tab will make
    user interface more readable.
    
    Let's,
    * change separator to '='
    * indent the chatbot's messages

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -1,13 +1,14 @@
 public class Yoyo {
     public static void main(String[] args) {
-        String separator = "____________________________" +
-                "________________________________";
+        String separator = "=========================" +
+                "===============================";
+        String tab = "    ";
 
-        System.out.println(separator);
-        System.out.println("Yo! The name's Yoyo.\nWhat can " +
-                "I do for you?");
-        System.out.println(separator);
-        System.out.println("Aww, bye! See ya later.");
-        System.out.println(separator);
+        System.out.println(tab + separator);
+        System.out.println(tab + "Yo! The name's Yoyo.\n"
+                + tab + "What can I do for you?");
+        System.out.println(tab + separator);
+        System.out.println(tab + "Aww, bye! See ya later.");
+        System.out.println(tab + separator);
     }
 }
