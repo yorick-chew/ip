d1fe33784174a396a8769aebaa3f7cdb250a6ec2
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-08-21 10:16:02 +0800

    Add 3 new task options for Yoyo to track
    Additional tasks options, to-do, deadline and event allow
    timing of the task to be tracked, making Yoyo more useful to
    its users.
    
    Let's,
    * track the number of tasks in Yoyo's list when adding tasks
    to it
    * update the list of commands to create 3 new types of tasks,
    to-dos, deadlines and events

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -4,9 +4,29 @@ public class Yoyo {
     private static Task[] taskLst = new Task[100];
     private static int currIdx = 0;
 
-    private static void addToLst(String text) {
-        taskLst[currIdx] = new Task(text);
+    private static Task addTask(String description) {
+        ToDo newToDo = new ToDo(description);
+        taskLst[currIdx] = newToDo;
         currIdx++;
+        return newToDo;
+    }
+
+    private static Task addTask(String description, String by) {
+        Deadline newDeadline = new Deadline(description, by);
+        taskLst[currIdx] = newDeadline;
+        currIdx++;
+        return newDeadline;
+    }
+
+    private static Task addTask(String description, String from, String to) {
+        Event newEvent = new Event(description, from, to);
+        taskLst[currIdx] = newEvent;
+        currIdx++;
+        return newEvent;
+    }
+
+    private static int numOfTasks() {
+        return currIdx;
     }
 
     public static void main(String[] args) {
@@ -56,11 +76,50 @@ public class Yoyo {
                                 tab + "Unmarked it for you:\n" + tab + "   " +
                                 Yoyo.taskLst[taskIdx]);
                 System.out.println(tab + separator);
-            } else {
-                // Yoyo adds the userPrompt to its list
+            } else if (userPrompt.equals("todo")) {
+                // Yoyo adds a new to-do to its list
+                String description = scanner.nextLine().substring(1);
+                Task newTask = Yoyo.addTask(description);
                 System.out.println(tab + separator);
-                Yoyo.addToLst(userPrompt);
-                System.out.println(tab + "added: " + userPrompt);
+                System.out.println(tab + "Alright-y, I've added your task:\n" +
+                        tab + "   " + newTask + "\n" + tab + "Now you have "
+                        + Yoyo.numOfTasks() + " tasks in the list.");
+                System.out.println(tab + separator);
+            } else if (userPrompt.equals("deadline")) {
+                // Yoyo adds a new deadline to its list
+                scanner.useDelimiter(" /by ");
+                String description = scanner.next().substring(1);
+                scanner.reset();
+                scanner.next();
+                String by = scanner.nextLine().substring(1);
+                Task newTask = Yoyo.addTask(description, by);
+                System.out.println(tab + separator);
+                System.out.println(tab + "Alright-y, I've added your task:\n" +
+                        tab + "   " + newTask + "\n" + tab + "Now you have "
+                        + Yoyo.numOfTasks() + " tasks in the list.");
+                System.out.println(tab + separator);
+            } else if (userPrompt.equals("event")) {
+                // Yoyo adds a new event to its list
+                scanner.useDelimiter(" /from ");
+                String description = scanner.next().substring(1);
+                scanner.reset();
+                scanner.next();
+                scanner.useDelimiter(" /to ");
+                String from = scanner.next().substring(1);
+                scanner.reset();
+                scanner.next();
+                String to = scanner.nextLine().substring(1);
+                Task newTask = Yoyo.addTask(description, from, to);
+                System.out.println(tab + separator);
+                System.out.println(tab + "Alright-y, I've added your task:\n" +
+                        tab + "   " + newTask + "\n" + tab + "Now you have "
+                        + Yoyo.numOfTasks() + " tasks in the list.");
+                System.out.println(tab + separator);
+            } else {
+                // The user's prompt is not preceded by a valid command
+                System.out.println(tab + separator);
+                System.out.println(tab + "Umm... What did you just say? " +
+                        "I genuinely don't\n" + tab + "understand...");
                 System.out.println(tab + separator);
             }
         }
